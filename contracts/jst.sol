392a72a91f4e7c8f20adec577048c86137d7db65
DaoThang <thangdao1983@gmail.com>
2023-12-04 11:48:24 +0700

    Add JST contract

---
+++ contracts/jst.sol
@@ -0,0 +1,335 @@
+// SPDX-License-Identifier: GPL-3.0
+// Source: https://tronscan.org/#/token20/TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9/code
+
+pragma solidity >=0.6.0;
+
+contract DSMath {
+    function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x, "ds-math-add-overflow");
+    }
+
+    function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x, "ds-math-sub-underflow");
+    }
+
+    function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+    }
+
+    function min(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        return x <= y ? x : y;
+    }
+
+    function max(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        return x >= y ? x : y;
+    }
+
+    function hadd(uint128 x, uint128 y) pure internal returns (uint128 z) {
+        require((z = x + y) >= x);
+    }
+
+    function hsub(uint128 x, uint128 y) pure internal returns (uint128 z) {
+        require((z = x - y) <= x);
+    }
+
+    function hmul(uint128 x, uint128 y) pure internal returns (uint128 z) {
+        require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+    }
+
+    function hmin(uint128 x, uint128 y) pure internal returns (uint128 z) {
+        return x <= y ? x : y;
+    }
+
+    function hmax(uint128 x, uint128 y) pure internal returns (uint128 z) {
+        return x >= y ? x : y;
+    }
+
+    function imin(int256 x, int256 y) internal pure returns (int256 z) {
+        return x <= y ? x : y;
+    }
+
+    function imax(int256 x, int256 y) internal pure returns (int256 z) {
+        return x >= y ? x : y;
+    }
+
+    uint256 constant WAD = 10 ** 18;
+    uint256 constant RAY = 10 ** 27;
+
+    function wmul(uint128 x, uint128 y) internal pure returns (uint128 z) {
+        z = cast((uint256(x) * y + WAD / 2) / WAD);
+    }
+
+    function whdiv(uint128 x, uint128 y) internal pure returns (uint128 z) {
+        z = cast((uint256(x) * WAD + y / 2) / y);
+    }
+
+    function wmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = add(mul(x, y), WAD / 2) / WAD;
+    }
+
+    function wdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = add(mul(x, WAD), y / 2) / y;
+    }
+
+    function rmul(uint128 x, uint128 y) internal pure returns (uint128 z) {
+        z = cast((uint256(x) * y + RAY / 2) / RAY);
+    }
+
+    function rdiv(uint128 x, uint128 y) internal pure returns (uint128 z) {
+        z = cast((uint256(x) * RAY + y / 2) / y);
+    }
+
+    function rmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = add(mul(x, y), RAY / 2) / RAY;
+    }
+
+    function rdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        z = add(mul(x, RAY), y / 2) / y;
+    }
+
+    function rpow(uint256 x, uint256 n) internal pure returns (uint256 z) {
+        z = n % 2 != 0 ? x : RAY;
+
+        for (n /= 2; n != 0; n /= 2) {
+            x = rmul(x, x);
+
+            if (n % 2 != 0) {
+                z = rmul(z, x);
+            }
+        }
+    }
+
+    function cast(uint256 x) internal pure returns (uint128 z) {
+        require((z = uint128(x)) == x);
+    }
+}
+
+abstract contract DSAuthority {
+    function canCall(address src, address dst, bytes4 sig) virtual public view returns (bool);
+}
+
+contract DSAuthEvents {
+    event LogSetAuthority (address indexed authority);
+    event LogSetOwner     (address indexed owner);
+}
+
+contract DSAuth is DSAuthEvents {
+    DSAuthority  public  authority;
+    address      public  owner;
+
+    constructor() {
+        owner = msg.sender;
+        emit LogSetOwner(msg.sender);
+    }
+
+    function setOwner(address owner_) public auth {
+        owner = owner_;
+        emit LogSetOwner(owner);
+    }
+
+    function setAuthority(DSAuthority authority_) public auth returns (bool result) {
+        authority = authority_;
+        emit LogSetAuthority(address(authority));
+        result = true;
+    }
+
+    modifier auth {
+        require(isAuthorized(msg.sender, msg.sig), "ds-auth-unauthorized");
+        _;
+    }
+
+    function isAuthorized(address src, bytes4 sig) internal view returns (bool) {
+        if (src == address(this)) {
+            return true;
+        } else if (src == owner) {
+            return true;
+        } else if (address(authority) == address(0)) {
+            return false;
+        } else {
+            return authority.canCall(src, address(this), sig);
+        }
+    }
+}
+
+contract DSNote {
+    event LogNote(
+        bytes4   indexed sig,
+        address  indexed guy,
+        bytes32  indexed foo,
+        bytes32  indexed bar,
+        uint256 sad,
+        bytes fax
+    ) anonymous;
+
+    modifier note {
+        bytes32 foo;
+        bytes32 bar;
+        uint256 sad;
+
+        assembly {
+            foo := calldataload(4)
+            bar := calldataload(36)
+            sad := callvalue()
+        }
+
+        emit LogNote(msg.sig, msg.sender, foo, bar, sad, msg.data);
+
+        _;
+    }
+}
+
+contract DSStop is DSNote, DSAuth {
+    bool public stopped;
+
+    modifier stoppable {
+        require(!stopped, "ds-stop-is-stopped");
+        _;
+    }
+    function stop() public auth note {
+        stopped = true;
+    }
+    function start() public auth note {
+        stopped = false;
+    }
+}
+
+contract TRC20Events {
+    event Approval(address indexed src, address indexed guy, uint wad);
+    event Transfer(address indexed src, address indexed dst, uint wad);
+}
+
+abstract contract TRC20 is TRC20Events {
+    function totalSupply() virtual public view returns (uint);
+    function balanceOf(address guy) virtual public view returns (uint);
+    function allowance(address src, address guy) virtual public view returns (uint);
+
+    function approve(address guy, uint wad) virtual public returns (bool);
+    function transfer(address dst, uint wad) virtual public returns (bool);
+    function transferFrom(address src, address dst, uint wad) virtual public returns (bool);
+}
+
+contract DSTokenBase is TRC20, DSMath {
+    uint256                                            _supply;
+    mapping(address => uint256)                       _balances;
+    mapping(address => mapping(address => uint256))  _approvals;
+
+    constructor(uint supply) {
+        _balances[msg.sender] = supply;
+        _supply = supply;
+    }
+
+    function totalSupply() override public view returns (uint) {
+        return _supply;
+    }
+
+    function balanceOf(address src) override public view returns (uint) {
+        return _balances[src];
+    }
+
+    function allowance(address src, address guy) override public view returns (uint) {
+        return _approvals[src][guy];
+    }
+
+    function transfer(address dst, uint wad) override public returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
+    }
+
+    function transferFrom(address src, address dst, uint wad) virtual override public returns (bool) {
+        if (src != msg.sender) {
+            require(_approvals[src][msg.sender] >= wad, "ds-token-insufficient-approval");
+            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        }
+
+        require(_balances[src] >= wad, "ds-token-insufficient-balance");
+        _balances[src] = sub(_balances[src], wad);
+        _balances[dst] = add(_balances[dst], wad);
+
+        emit Transfer(src, dst, wad);
+
+        return true;
+    }
+
+    function approve(address guy, uint wad) virtual override public returns (bool) {
+        _approvals[msg.sender][guy] = wad;
+        emit Approval(msg.sender, guy, wad);
+        return true;
+    }
+}
+
+contract DSToken is DSTokenBase(0), DSStop {
+    string  public  symbol;
+    uint256  public  decimals = 18; // standard token precision. override to customize
+
+    constructor(string memory symbol_) {
+        symbol = symbol_;
+    }
+
+    event Mint(address indexed guy, uint wad);
+    event Burn(address indexed guy, uint wad);
+
+    function approve(address guy) public stoppable returns (bool) {
+        return super.approve(guy, type(uint).max);
+    }
+
+    function approve(address guy, uint wad) override public stoppable returns (bool) {
+        return super.approve(guy, wad);
+    }
+
+    function transferFrom(address src, address dst, uint wad) override public stoppable returns (bool) {
+        if (src != msg.sender && _approvals[src][msg.sender] != type(uint).max) {
+            require(_approvals[src][msg.sender] >= wad, "ds-token-insufficient-approval");
+            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        }
+
+        require(_balances[src] >= wad, "ds-token-insufficient-balance");
+        _balances[src] = sub(_balances[src], wad);
+        _balances[dst] = add(_balances[dst], wad);
+
+        emit Transfer(src, dst, wad);
+        return true;
+    }
+
+    function push(address dst, uint wad) public {
+        transferFrom(msg.sender, dst, wad);
+    }
+    function pull(address src, uint wad) public {
+        transferFrom(src, msg.sender, wad);
+    }
+    function move(address src, address dst, uint wad) public {
+        transferFrom(src, dst, wad);
+    }
+
+    function mint(uint wad) public {
+        mint(msg.sender, wad);
+    }
+    function burn(uint wad) public {
+        burn(msg.sender, wad);
+    }
+    function mint(address guy, uint wad) public auth stoppable {
+        _balances[guy] = add(_balances[guy], wad);
+        _supply = add(_supply, wad);
+        emit Mint(guy, wad);
+    }
+    function burn(address guy, uint wad) public auth stoppable {
+        if (guy != msg.sender && _approvals[guy][msg.sender] != type(uint).max) {
+            require(_approvals[guy][msg.sender] >= wad, "ds-token-insufficient-approval");
+            _approvals[guy][msg.sender] = sub(_approvals[guy][msg.sender], wad);
+        }
+
+        require(_balances[guy] >= wad, "ds-token-insufficient-balance");
+        _balances[guy] = sub(_balances[guy], wad);
+        _supply = sub(_supply, wad);
+        emit Burn(guy, wad);
+    }
+
+    // Optional token name
+    string   public  name = "";
+
+    function setName(string memory name_) public auth {
+        name = name_;
+    }
+    // Optional symbol name
+    function setSymbol(string memory symbol_) public auth {
+        symbol = symbol_;
+    }
+}
