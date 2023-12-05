aadac52d448d8e7c4b31cb9082549749aa834d81
DaoThang <thangdao1983@gmail.com>
2023-12-05 11:36:32 +0700

    Update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,19 @@
-// var MyContract = artifacts.require("./MyContract.sol");
+var JST = artifacts.require("./jst.sol");
+var SUN = artifacts.require("./sun.sol");
+var TUSD = artifacts.require("./tusd.sol");
+var USDC = artifacts.require("./usdc.sol");
+var USDD = artifacts.require("./usdd.sol");
+var USDT = artifacts.require("./usdt.sol");
+var WIN = artifacts.require("./win.sol");
+var WTRX = artifacts.require("./wtrx.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(MyContract);
+    deployer.deploy(JST);
+    deployer.deploy(SUN);
+    deployer.deploy(TUSD);
+    deployer.deploy(USDC);
+    deployer.deploy(USDD);
+    deployer.deploy(USDT);
+    deployer.deploy(WIN);
+    deployer.deploy(WTRX);
 };
